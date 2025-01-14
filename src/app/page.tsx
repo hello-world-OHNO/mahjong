d6b738bde7d2a4dd88a34b6d5f0490e0875c8fae
hello-world-OHNO <daruma0248@gmail.com>
2025-01-14 19:10:19 +0900

    feat:環境構築

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+export default function Home() {
+  return (
+    <Box>
+      <Typography>麻雀</Typography>
+    </Box>
+  );
+}
